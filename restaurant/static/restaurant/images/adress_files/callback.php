d18848d549d69c424001a2b0aaf64fa039b9574b
Iliazv <117463697+Iliazv@users.noreply.github.com>
2022-12-08 09:54:38 +0700

    Add files via upload

---
+++ restaurant/static/restaurant/images/adress_files/callback.php
@@ -0,0 +1,13 @@
+
+bookbanket.addCallback('activation', function(detail) {
+    
+  });
+bookbanket.addCallback('form_opening', function(detail) {
+    
+  });
+bookbanket.addCallback('registration', function(detail) {
+    
+  });
+bookbanket.addCallback('cost_calculation_request', function(detail) {
+    
+  });
